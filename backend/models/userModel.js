5743826f17071a7023a9db19f4dbfb08998a6586
BeeJayTee <91142693+BeeJayTee@users.noreply.github.com>
2023-04-04 10:33:57 -0400

    backend user auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,4 +15,49 @@ const userSchema = new Schema({
   },
 });
 
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("all fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("not a valid email address");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("password not strong enough");
+  }
+
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
+
+// login stastic method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("all fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("invalid login attempt");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("invalid login attempt");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 import List from "./components/List";
 import AddItem from "./components/AddItem";
+import Login from "./pages/login";
 
 function App() {
   return (
     <div className="App">
-      <div className="container m-auto mt-8">
-        <AddItem />
-        <div>
-          <List />
-        </div>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <div className="container m-auto mt-8">
+                <AddItem />
+                <div>
+                  <List />
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
